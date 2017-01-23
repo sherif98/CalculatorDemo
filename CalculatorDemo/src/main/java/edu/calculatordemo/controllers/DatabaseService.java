47fc632af33f9ad5756f28643f57da9501934bca
sherif98 <sheco_12345@hotmail.com>
2017-01-23 22:53:38 +0200

    added controllers skeleton

--- CalculatorDemo/src/main/java/edu/calculatordemo/controllers/DatabaseService.java
+++ CalculatorDemo/src/main/java/edu/calculatordemo/controllers/DatabaseService.java
@@ -1,5 +1,10 @@
 package edu.calculatordemo.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/history")
 public class DatabaseService {
 
 }
