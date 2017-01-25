491f4a2ddfd7ea774671c2e1d4ae722e0a766261
sherif98 <sheco_12345@hotmail.com>
2017-01-25 13:45:37 +0200

    init commit

---
+++ front end/CalculatorDemoFrontEnd/src/test/java/edu/calculator/demo/frontend/CalculatorDemoFrontEndApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.calculator.demo.frontend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorDemoFrontEndApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
