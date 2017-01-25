83555cb6287b84c9e89ec42f9f7bc73e7d4ca56e
sherif98 <sheco_12345@hotmail.com>
2017-01-25 13:45:09 +0200

    init commit

---
+++ front end/CalculatorDemoFrontEnd/src/main/java/edu/calculator/demo/frontend/CalculatorDemoFrontEndApplication.java
@@ -0,0 +1,13 @@
+package edu.calculator.demo.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorDemoFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorDemoFrontEndApplication.class, args);
+		//
+	}
+}
